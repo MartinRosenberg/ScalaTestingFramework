83f49264194bd874cbc1f6941c215e42c5944e93
Martin Rosenberg <martin.rosenberg@backstopsolutions.com>
2017-03-01 23:34:09 -0600

    Add todos

--- src/test/scala-2.12/com/martinbrosenberg/fw/test/Spec.scala
+++ src/test/scala-2.12/com/martinbrosenberg/fw/test/Spec.scala
@@ -12,9 +12,12 @@ class Spec extends BaseSpec {
   "The shortened URL is the same in the field as the history below for" - {
     testCases foreach { case (site, original, _) =>
       site in {
+        // todo not a fan of having to `new` things in a spec, even with this sugar
         val bhp = driver.get(new BitlyHome)
         bhp.field.sendKeys(original + timestamp)
+        // todo this will fail without waiting
         bhp.button.click()
+        // todo `getAttribute` and `getText` are very un-Scala-like names -- wrap?
         val shortUrlField = bhp.field.getAttribute("value")
         val shortUrlHistory = bhp.mostRecentLink.getText
         shortUrlField shouldEqual shortUrlHistory
